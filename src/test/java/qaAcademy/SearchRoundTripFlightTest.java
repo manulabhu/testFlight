67f1347cf32c1c2a221bff1b0261d7cc6ebb0ea5
manulabhu <manulabhu@gmail.com>
2021-08-20 13:05:49 -0500

    t'

--- src/test/java/qaAcademy/SearchRoundTripFlightTest.java
+++ src/test/java/qaAcademy/SearchRoundTripFlightTest.java
@@ -104,7 +104,7 @@ public class SearchRoundTripFlightTest extends base {
 		//w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@value='STV'])[2]")));
 		fl.getToCity().click();
 		log.debug("To city is selected");
-		
+		System.out.println("Test");
 		
 	}
 	
